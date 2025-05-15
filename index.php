b25752d3c9bb977d41750dd15f7f804a17a62730
NirajTharu <nirajchaudhary@Book.local>
2025-05-15 09:54:12 +0545

    This is for initializing

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "We are messing with the computers.Find me the good one in the planet.
+
+Is something Wrong. What do you mean? This tag just contain the  content  of he paragraph. I know this much only in the field of this.\n";
