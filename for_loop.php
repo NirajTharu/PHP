7fe088cab78dfcb96cf10a9535c86b0c7871fc4b
NirajTharu <nirajchaudhary@Book.local>
2025-05-17 09:02:33 +0545

    about the for loop

---
+++ for_loop.php
@@ -0,0 +1,9 @@
+<?php
+// For loop
+// This will repeat some amount of code # times
+// For increment by one
+// For increment by two for ($i = 0; $i < 5; $i+=2) 
+for ($i = 0; $i < 33; $i++) {
+    echo $i . "<br>";
+}
+?>
