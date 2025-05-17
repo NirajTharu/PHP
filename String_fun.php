0ff9c48a5cd4ca2a44be6b36d2ebc39df714cd88
NirajTharu <nirajchaudhary@Book.local>
2025-05-17 18:53:03 +0545

    Some of the string funciton in php

---
+++ String_fun.php
@@ -0,0 +1,49 @@
+<?php
+// Few useful functions in php
+
+$username = "Niraj kumar Chaudhary ";
+
+// $username = strtolower($username);
+// $username = strtoupper($username);
+// This will remove white space after the sting
+// $username = trim($username);
+// This will add some character in this case "i" four times
+// $username = str_pad($username, 30, "i");
+
+// $phone = "222 - 993 - 938";
+// $phone = str_replace("-", "/", "$phone");
+
+//Reverse
+// $username = strrev($username);
+
+// Shuffle
+// $username = str_shuffle($username);
+
+// compare
+// $equals = strcmp($username, "Niraj Chaudhary");
+
+// For the length of string
+// $username = strlen($username);
+
+// For the position of a alphabet
+$username = strpos($username, " ");
+
+// For creating the substring from the original one
+// $username = substr($username, "0", "7");
+
+// For creating substring after that
+// $username = substr($username, 3);
+
+// For the dividing the array
+// You should mention how to explode like i have used the " "(white space)
+// $username = explode(" ", $username);
+
+// and  implode function for the opposite of the above one
+
+
+// echo $phone;
+
+// for compare
+// echo $equals;
+
+echo $username;
