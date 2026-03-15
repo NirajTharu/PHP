9fe73f10bceddb5faafc23fe97129359bb4c9764
NirajTharu <nirajc279@gmail.com>
2026-03-16 00:13:10 +0545

    Adding the file of the database

--- Db.php
+++ Db.php
@@ -6,8 +6,10 @@
 $db_SERVER ="localhost";
 $db_user ="root";
 $db_pass="root";
-$db_name="Business_Db";
+$db_name="Business_DB";
 $conn="";
+$conn = mysqli_connect($db_SERVER, $db_user, $db_pass, $db_name);
+
 try{
     $conn=mysqli_connect("localhost","root","root","Business_Db");
 }
