e780121a7909738833d5d51e10bcca637bc12ccc
NirajTharu <nirajc279@gmail.com>
2026-03-16 00:47:52 +0545

    Adding the code snippits which was not done at that time

--- index1.php
+++ index1.php
@@ -1,5 +1,23 @@
 <?php
-include("Db.php");
+    include("Db.php");
+    
+    $username = "Timro";
+    $passwords = "Timi11";
+
+    $hash = password_hash('$passwords', PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO User_Details(user, passwords)  
+    VALUES ('$username', '$hash' )";
+    try{
+    mysqli_query($conn, $sql);
+    echo "User is registered.";
+    }
+    catch(mysqli_sql_exception){
+    echo "Could not registerd the user.";
+    }
+
+
+mysqli_close($conn);
 
 ?>
 <!DOCTYPE html>
@@ -10,7 +28,7 @@ include("Db.php");
     <title>Document</title>
 </head>
 <body>
-    Hi<br>
+    Hi there. <br>
 
 </body>
 </html>
